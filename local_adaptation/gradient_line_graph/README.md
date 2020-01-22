cfc849f597aa1c0e72a53aef930facb402741ca4
jcbain <jamescbain@gmail.com>
2020-01-21 18:24:32 -0700

    make responsive with viewbox

---
+++ local_adaptation/gradient_line_graph/README.md
@@ -0,0 +1,7 @@
+## Helpful Resources
+
+1. [Reponsiveness in d3.js](https://www.d3-graph-gallery.com/graph/custom_responsive.html) was a helpful piece in one way to create a chart that was responsive to window resizing with the addition of an event listener.
+2. [Line plot with several groups](https://www.d3-graph-gallery.com/graph/line_several_group.html) gave a good information on how to configure a line chart with multiple groups.
+3. [Using gradients for abrupt color changes in data visualizations](https://www.visualcinnamon.com/2016/06/abrupt-color-gradients) was extremely helpful in pointing to how one could color parts of an svg using gradients.
+4. [Brush Handles](https://observablehq.com/@d3/brush-handles) provided both incite into how to set up a viewbox for responsive web design as well as how to do brushes.
+5. [How to Scale SVG](https://css-tricks.com/scale-svg/) discusses the concepts of viewbox.
--- local_adaptation/gradient_line_graph/js/linechart_viewbox.js
+++ local_adaptation/gradient_line_graph/js/linechart_viewbox.js
@@ -9,13 +9,10 @@ Array.prototype.unique = function() {
 const popSize = 1000;
 
 // PLOTTING VARIABLES
-let chartWidth = 80,
-    chartHeight = 20,
+let chartWidth = 800,
+    chartHeight = 100,
     padding = 10;
 
-// let chartWidth = 800,
-//     chartHeight = 40,
-//     padding = 10;
 
 d3.json("data/mutations_bg.json").then( data => {
 
@@ -53,9 +50,8 @@ d3.json("data/mutations_bg.json").then( data => {
 
     let svg = d3.select('#line-chart')
         .append('svg')
-        .attr('width', chartWidth + "vw")
-        .attr('height', chartHeight + "vh");
-    
+        .attr("viewBox", [0, 0, chartWidth, chartHeight]);
+
     // define the scales
     let xScale = d3.scaleLinear()
         .domain([d3.min(dataPopPhen, d => d.output_gen),
@@ -78,57 +74,34 @@ d3.json("data/mutations_bg.json").then( data => {
         .key( d => d.pop )
         .entries(dataFiltered);
 
+
     let popKeys = dataGrouped.map( d => d.key );
     let color = d3.scaleOrdinal()
         .domain(popKeys)
         .range(['#e41a1c','#377eb8','#4daf4a']);
 
-    // set up gradients with ids for each population in the popKeys array
-    // so that each population gets its own color associated with it
     let gradients = svg.selectAll('defs')
         .data(popKeys)
         .enter()
         .append('linearGradient')
-        .attr('id', d => 'gradient_pop_' + d);
+        .attr('id', d => 'gradient_pop_' + d)
+        .attr('x1', 0)
+        .attr('y1', 0)
+        .attr('x2', '100%')
+        .attr('y2', 0);
 
     // define start and stop colors for gradient
-    let startGrey = gradients.append('stop').attr('stop-color', '#D6D6D6');
-    let startColor = gradients.append('stop').attr('stop-color', d => color(d));
-    let endColor = gradients.append('stop').attr('stop-color', d => color(d));
-    let endGrey = gradients.append('stop').attr('stop-color', '#D6D6D6');
+    let startGrey = gradients.append('stop').attr('stop-color', '#D6D6D6').attr("class", "left").attr("offset", "40%");
+    let startColor = gradients.append('stop').attr('stop-color', d => color(d)).attr("class", "left").attr("offset", "40%");
+    let endColor = gradients.append('stop').attr('stop-color', d => color(d)).attr("class", "right").attr("offset", "60%");
+    let endGrey = gradients.append('stop').attr('stop-color', '#D6D6D6').attr("class", "right").attr("offset", "60%");
 
-    // create paths
     let line = svg.selectAll('.line')
         .data(dataGrouped)
         .enter()
         .append('path')
         .attr('fill', 'none')
-        .attr('stroke-width', 1.5);
-
-
-
-    // function to find current width and height of chart container.
-    function drawResponsiveChart(){
-        let currentWidth = parseInt(svg.style('width'), 10);
-        let currentHeight = parseInt(svg.style('height'), 10);
-
-
-        xScale.range([padding, currentWidth - padding]);
-        yScale.range([currentHeight - padding, padding]);
-
-        // linearGradient.attr("gradientUnits", "userSpaceOnUse")
-        gradients.attr('gradientUnits', 'userSpaceOnUse')
-            .attr('x1', 0)
-            .attr('y1', 0)
-            .attr('x2', currentWidth)
-            .attr('y2', 0);
-
-        startGrey.attr("class", "left").attr("offset", "40%");
-        startColor.attr("class", "left").attr("offset", "40%");
-        endColor.attr("class", "right").attr("offset", "60%");
-        endGrey.attr("class", "right").attr("offset", "60%");
-
-        line
+        .attr('stroke-width', 1.5)
         .attr('stroke', d => 'url(#gradient_pop_' + d.key +')')
         .attr('d', function(d){
             return d3.line()
@@ -137,13 +110,6 @@ d3.json("data/mutations_bg.json").then( data => {
                 (d.values)
         });
 
-	
-    };
 
-    // initialize the chart
-    drawResponsiveChart()
-
-    // adjust the chart on resize
-    window.addEventListener('resize', drawResponsiveChart );
 
 });
