7f56a3ec04c31f18f212e9843191514c0f1d8c95
jcbain <jamescbain@gmail.com>
2020-01-28 23:57:37 -0700

    include aarray function

--- local_adaptation/gradient_genome_plot/js/genome.js
+++ local_adaptation/gradient_genome_plot/js/genome.js
@@ -6,7 +6,8 @@ Array.prototype.unique = function() {
 
 // PLOTTING VARIABLES
 let chartWidth = 800,
-    chartHeight = 400;
+    chartHeight = 200
+    chromeRounding = 20;
 
 let margin = {top: 10, right: 20, bottom: 20, left: 20};
 
@@ -21,15 +22,39 @@ Promise.all([
         d['positional_phen'] = d.freq * d.select_coef;
     })
 
+    let mOpts = sampleFunct(data, [], 'm');
+    let muOpts = sampleFunct(data, [], 'mu');
+    let rOpts = sampleFunct(data, [], 'r');
+    let sigsqrOpts = sampleFunct(data, [], 'sigsqr');
+    let outputGenOpts = sampleFunct(data, [], 'ouptput_gen');
+    let popOpts = sampleFunct(data, [], 'pop');
+
+
+
     console.log(d3.min(data, d => Math.abs(d.positional_phen)));
     console.log(d3.max(data, d => Math.abs(d.positional_phen)));
 
+    d3.select('#genome-chart')
+        .append('div')
+        .attr('id', 'migration-selector')
+        .attr('class', 'param-opts')
+        .selectAll('.migration-opts')
+        .data(mOpts)
+        .enter()
+        .append('button')
+        .attr('id', d => 'opt' + d)
+        .attr('class', 'migration-opts')
+        .attr('value', d => d)
+        .text(d => d);
+
+    initialParams = {mu: "1e-6", r: "1e-6", sigsqr: "5", m: "1e-3", output_gen: 50000, pop: 0}
 
 
+    // let dataFiltered = data.filter(function(d){
+    //     return d.mu == "1e-6" && d.r == "1e-6" && d.sigsqr == "5" && d.m == "1e-3" && d.output_gen == 50000 && d.pop == 0;
+    // }) 
 
-    let dataFiltered = data.filter(function(d){
-        return d.mu == "1e-6" && d.r == "1e-6" && d.sigsqr == "5" && d.m == "1e-3" && d.output_gen == 50000 && d.pop == 0;
-    }) 
+    let dataFiltered = data.filter(d => filterOnParams(d, initialParams.mu, initialParams.r, initialParams.sigsqr, initialParams.m, initialParams.output_gen, initialParams.pop));
 
     // TODO: I need to figure out how to handle mutations at the same
     //       location. Right now, it is just taking the first one, which is rather
@@ -76,14 +101,20 @@ Promise.all([
         .enter()
         .append('stop').attr('stop-color', d => {
             if( d.positional_phen > 0){
-                return 'red'
+                return '#ba3252'
             } else if(d.positional_phen < 0){
-                return 'blue'
+                return '#3277a8'
             } else {
-                return 'white'
+                return '#fffff7'
             }
         })
-        .attr('stop-opacity', d => opacityScale(Math.abs(d.positional_phen)) + "%")
+        .attr('stop-opacity', d => {
+            if (d.positional_phen == 0) {
+                return "100%"
+            } else{
+                return opacityScale(Math.abs(d.positional_phen)) + "%"
+            }
+            })
         .attr('offset', (d, i) => yScale(i) + "%");
 
 
@@ -92,12 +123,21 @@ Promise.all([
         .attr('class', 'chrome')
         .attr('x', chartWidth/2)
         .attr('y', 0)
+        .attr('rx', chromeRounding)
+        .attr('ry', chromeRounding)
         .attr('height', chartHeight - margin.bottom)
         .attr('width', 40)
-        .attr('stroke', 'green')
+        .attr('stroke', '#c2c2ab')
         .style('fill', "url(#grads)");
 
-
-
-
 })
+
+// create a function to filter data
+function filterOnParams(row, mu, r, sigsqr, m, output_gen, pop) {
+    return row.mu === mu && row.m === m && row.r === r && row.sigsqr === sigsqr && row.output_gen === output_gen && row.pop === pop;
+};
+
+function sampleFunct(data, array, m){
+    data.forEach(d => array.push(d[m]));
+    return array.unique();
+}
