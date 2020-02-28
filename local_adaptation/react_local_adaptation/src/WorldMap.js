1f2cb4624076e9cc213d3172a332749d8b7ee279
jcbain <jamescbain@gmail.com>
2020-02-28 14:25:47 -0700

    make data reactive

--- local_adaptation/react_local_adaptation/src/WorldMap.js
+++ local_adaptation/react_local_adaptation/src/WorldMap.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import worlddata from './world';
 import { geoMercator, geoPath } from 'd3-geo';
 
 class WorldMap extends Component {
     render() {
-        const projection = geoMercator();
+        const projection = geoMercator()
+            .scale(120)
+            .translate([430, 250]);
+
         const pathGenerator = geoPath().projection(projection);
 
-        const countries = worlddata.features
+        const countries = this.props.data
             .map((d, i) => <path 
                 key={'path' + i}
                 d={pathGenerator(d)}
+                style={{fill: this.props.colorScale(d.launchday),
+                        stroke: 'black', strokeOpacity: 0.5 }}
                 className='countries'
                 />);
 
-        return <svg width={500} height={500}>
+        return <svg width={this.props.size[0]} height={this.props.size[1]}>
                     {countries}
                 </svg>;
     }
