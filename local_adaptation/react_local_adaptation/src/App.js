86679cc6c1d4d9c21d9417d570fc14ba6d083b5d
jcbain <jamescbain@gmail.com>
2020-02-28 23:11:00 -0700

    update barchart

--- local_adaptation/react_local_adaptation/src/App.js
+++ local_adaptation/react_local_adaptation/src/App.js
@@ -3,34 +3,52 @@ import './App.css'
 import WorldMap from './WorldMap'
 import worlddata from './world'
 import BarChart from './BarChart'
-import { range, sum } from 'd3-array'
+import StreamGraph from './StreamGraph'
+import { range } from 'd3-array'
 import { scaleThreshold } from 'd3-scale'
 import { geoCentroid} from 'd3-geo'
 
 const appdata = worlddata.features
   .filter(d => geoCentroid(d)[0] < -20);
 
-  appdata
-    .forEach((d,i) => {
-      const offset = Math.random();
-      d.launchday = i;
-      d.data = range(30).map((p,q) =>
-        q < i ? 0 : Math.random() * 2 + offset) 
-    }
-  ); 
+
+appdata
+  .forEach((d,i) => {
+    const offset = Math.random()
+    d.launchday = i
+    d.data = range(30).map((p,q) => q < i ? 0 : Math.random() * 2 + offset)
+  }
+);
+
+const colorScale = scaleThreshold().domain([5,10,20,30]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464"])
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onResize = this.onResize.bind(this);
+    this.state = { screenWidth: 1000, screenHeigght: 500};
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
+  }
+
+  onResize() {
+    this.setState({ screenWidth: window.innerWidth, 
+                    screenHeight: window.innerHeight - 70})
+  }
+
    render() {
-      const colorScale = scaleThreshold().domain([5, 10, 20, 30, 50])
-        .range(["#75739F", "#5EAFC6", "#41A368", "#93C464", "#FE9922"]);
    return (
      <div className="App">
        <div className="App-header">
          <h2>Dashboard</h2>
        </div>
        <div>
-         <WorldMap colorScale={colorScale} data={appdata} size={[500,400]}/>
-         <BarChart colorScale={colorScale} data={appdata} size={[500,400]} />
+          <StreamGraph colorScale={colorScale} data={appdata} size={[this.state.screenWidth, this.state.screenHeight / 2]} />
+          <WorldMap colorScale={colorScale} data={appdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]}/>
+          <BarChart colorScale={colorScale} data={appdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]} />
        </div>
      </div>
    )
