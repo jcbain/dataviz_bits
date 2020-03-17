4a5ec9fb7eb816fedf3d281ed48bfd9133a85365
jcbain <jamescbain@gmail.com>
2020-03-16 22:21:56 -0600

    create a resources page

--- local_adaptation/react_local_adaptation/src/App.js
+++ local_adaptation/react_local_adaptation/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { Route, NavLink, Switch } from "react-router-dom";
 
+import './App.css';
+
 import  Home  from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
+import Resources from './pages/Resources';
 import Collections from './pages/Collections';
 
 import LocalAdaptation from './pages/collections/LocalAdaptation';
@@ -12,22 +14,27 @@ import LocalAdaptation from './pages/collections/LocalAdaptation';
 class App extends Component {
   render() {
     return (
+      <div class="main-wrapper">
         <header>
           <h1>Atlas of Population Genetics</h1>
+          <div className="nav-links">
             <p><NavLink to="/">Home</NavLink></p>
             <p><NavLink to="/about">About</NavLink></p>
-            <p><NavLink to="/contact">Contact</NavLink></p>
+            <p><NavLink to="/resources">Resources</NavLink></p>
             <p><NavLink to="/collections">Collection</NavLink></p>
-          <div className="content">
+          </div>
+        </header>
+        <div className="content">
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/about" component={About}/>
-                <Route path="/contact" component={Contact}/>
+                <Route path="/resources" component={Resources}/>
                 <Route exact path="/collections" component={Collections}/>
-                <Route path="/collections/localadaptation" component={ LocalAdaptation }/>
+                <Route refresh={true} path="/collections/localadaptation" component={ LocalAdaptation }/>
             </Switch>
-          </div>
-        </header>
+        </div>
+        </div>
+
     );
   }
 }
--- local_adaptation/react_local_adaptation/src/pages/Resources.js
+++ local_adaptation/react_local_adaptation/src/pages/Resources.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Contact(){
+function Resources(){
     return(
         <div>
-            <h1>Contact</h1>
+            <h1>Resources</h1>
         </div>
     )
 }
 
-export default Contact;
+export default Resources;
