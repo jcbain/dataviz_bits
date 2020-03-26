2ead8d1099761864585856e33eaae083f3077746
jcbain <jamescbain@gmail.com>
2020-03-25 19:39:36 -0600

    update and fix

--- local_adaptation/react_local_adaptation/src/charts/ScrollingGraphic.js
+++ local_adaptation/react_local_adaptation/src/charts/ScrollingGraphic.js
@@ -1,41 +1,63 @@
 import React, { Component } from 'react';
 import { Scrollama, Step } from 'react-scrollama';
 
+import './styles/scrolling_graphic_styles.css';
+
 import { scaleLinear } from 'd3-scale';
 import { interpolateHcl } from 'd3-interpolate';
-import { easeBounce, easeSinInOut } from 'd3-ease';
+import { easeSinInOut } from 'd3-ease';
 import { select, selectAll } from 'd3-selection';
 import { min, max } from 'd3-array';
 import { nest } from 'd3-collection';
 import sort from 'fast-sort'
-import Slider from '@material-ui/core/Slider';
+
+import RangeSlider from '../components/RangeSlider';
+
 import individualData from '../data/individuals_small';
  
-import './styles/scrolling_graphic_styles.css';
-
 class Graphic extends Component {
     constructor(props){
         super(props);
         this.squareSize = 10;
-        this.numCols = 25;
+        this.numCols = 40;
         this.individualPadding = 1.2
+        this.popMargin = 50;
         this.genCounts = individualData.map(d => d.pop).filter(unique).map(v => countIndividaulsPerGeneration(individualData, v))
         this.populations = individualData.map(d => d.pop).filter(unique)
         this.maxPopVal = maxPerPop(this.genCounts);
+        this.minPhen = min(individualData, d => d.ind_phen)
+        this.maxPhen = max(individualData, d => d.ind_phen)
+        this.phenValues = [this.minPhen, this.maxPhen];
         this.colorScale = scaleLinear()
-                            .domain([-1, 0, 1])
+                            .domain([this.minPhen, 0, this.maxPhen])
                             .range(['#C38D9E', '#fffff7', '#E27D60'])
                             .interpolate(interpolateHcl);
+        this.handleChange = this.handleChange.bind(this);
 
         this.state = {
             data: 1000,
             steps: [10000, 20000, 50000],
             progress: 0,
             outputGen: 1000,
+            phenValues: this.phenValues
           };
     }
 
       popRef = React.createRef();
+
+      handleChange(newValue){
+        this.setState({phenValues: newValue})
+        select(this.popRef.current)
+          .selectAll('.pop_rects')
+          .transition()
+          .attr('opacity', d=>{
+            if(d.ind_phen >= newValue[0] && d.ind_phen <= newValue[1]){
+              return 1.0;
+            } else {
+              return .25;
+            }
+          })
+      }
 
       createData(){
         const numCols = this.numCols;
@@ -88,17 +110,15 @@ class Graphic extends Component {
       }
 
       componentDidMount(){
-          console.log('this here')
-          console.log(max(Object.values(this.maxPopVal)))
-          // console.log(this.createData())
-          console.log(this.state)
           select(this.popRef.current)
             .selectAll('.pop_rects')  
             .data(this.createData())
             .enter()
             .append('rect')
             .attr('class', 'pop_rects')
-            .attr('x', (d, i) => (((d.x) * this.squareSize ) ))
+            .attr('x', (d, i) => {
+              return ((d.x * (this.individualPadding))* this.squareSize)
+            })
             .attr('y', d => d.y * this.squareSize)
             .attr('rx', 2)
             .attr('ry', 2)
@@ -106,6 +126,11 @@ class Graphic extends Component {
             .attr('width', this.squareSize)
             .attr('fill', d => this.colorScale(d.ind_phen))
           
+      }
+
+      componentDidUpdate(){
+        console.log('this state')
+        console.log(this.state)
       }
     
       onStepEnter = ({ element, data}) => {
@@ -118,18 +143,25 @@ class Graphic extends Component {
           .append('rect')
           .attr('class', 'pop_rects')
           .attr('x', (d, i) => {
-            return ((d.x * this.individualPadding)* this.squareSize) + d.pop * 0;
+            return ((d.x * this.individualPadding) * this.squareSize) + d.pop * this.popMargin;
           })
           .attr('y', d => (d.y * this.individualPadding) * this.squareSize)
           .attr('rx', 2)
           .attr('ry', 2)
+          .attr('opacity', d=>{
+            if(d.ind_phen >= this.state.phenValues[0] && d.ind_phen <= this.state.phenValues[1]){
+              return 1.0;
+            } else {
+              return .25;
+            }
+           })
           .transition()
           .attr('height', this.squareSize)
           .attr('width', this.squareSize)
           
           .attr('fill', d => this.colorScale(d.ind_phen))
           .duration(1000)
-          .ease(easeSinInOut)
+          .ease(easeSinInOut); 
       };
     
       onStepExit = ({ element }) => {
@@ -154,20 +186,31 @@ class Graphic extends Component {
     
         return (
           <div className="scroller-main">
-            <svg className="scroller-graphic"
-                 viewBox={[0,
-                           0, 
-                           (this.squareSize * this.numCols * this.individualPadding * 2)  - this.individualPadding, 
-                           (max(Object.values(this.maxPopVal))/ this.numCols) * this.individualPadding * this.squareSize]}
-                 >  
-                <svg x="0" 
-                     y="0" 
-                     viewBox={[0, 0, (this.squareSize * this.numCols * this.individualPadding * 2) - this.individualPadding, (max(Object.values(this.maxPopVal))/ this.numCols) * this.individualPadding * this.squareSize]} 
-                     preserveAspectRatio="xMinYMid meet" 
-                     ref={this.popRef}
-                     className="pop-graphic"></svg>
-                <text x="20" y="35" className="small">{data}</text>
-            </svg>
+            <div className="scroller-graphic">
+              <svg 
+                  viewBox={[0,
+                            0, 
+                            (this.squareSize * this.numCols * this.individualPadding * 2)  - this.individualPadding + this.popMargin, 
+                            ((max(Object.values(this.maxPopVal))/ this.numCols) * this.individualPadding * this.squareSize) + 100]}
+                  >  
+                  <svg x="0" 
+                      y="0" 
+                      viewBox={[0, 
+                                0, 
+                                (this.squareSize * this.numCols * this.individualPadding * 2) - this.individualPadding + this.popMargin, 
+                                ((max(Object.values(this.maxPopVal))/ this.numCols) * this.individualPadding * this.squareSize) + 100]} 
+                      preserveAspectRatio="xMinYMid meet" 
+                      ref={this.popRef}
+                      className="pop-graphic"></svg>
+                    <text x="20" y="280" className="param-label">GENERATION</text>
+                    <text x="15" y="350" className="generation-text">{data}</text>
+              </svg>
+              <RangeSlider min={this.phenValues[0]} max={this.phenValues[1]}
+                           onSliderChange={this.handleChange}
+              />
+              
+            </div>
+
             <div className="scroller">
               <Scrollama
                 onStepEnter={this.onStepEnter}
@@ -175,7 +218,7 @@ class Graphic extends Component {
                 progress
                 onStepProgress={this.onStepProgress}
                 offset={0.25}
-                debug
+                // debug
               >
                 <Step data={10000}>
                   <div className="scroller-step">
@@ -194,14 +237,6 @@ class Graphic extends Component {
                     <p>And at 50,000th generation</p>
                   </div>
                 </Step>
-                {/* {steps.map(value => (
-                  <Step data={value} key={value}>
-                    <div className="scroller-step">
-                      <p>step value: {value}</p>
-                      <p>{value === data && progress}</p>
-                    </div>
-                  </Step>
-                ))} */}
               </Scrollama>
             </div>
 
